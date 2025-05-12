922457dbf03f24a67f8f8c77771f125fcfd63966
Leonardo <javascriptleonardolucas@gmail.com>
2025-05-12 20:44:47 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,4 +1,10 @@
+import sys
+
 meses = ["Jan", "Fev", "Marc", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
 numeroDoMes = int(input("mes de qual numero voce quer"))
+
+if (numeroDoMes > 12):
+    print("coloque um mes valido ")
+    sys.exit(1)
 
 print(meses[numeroDoMes - 1])
